4df9359ff75915a57e54c554037d0b1681b417ee
Hycarus <mirkomarasco08@gmail.com>
2023-10-06 14:34:12 +0200

    added new const testo

--- js/script.js
+++ js/script.js
@@ -3,5 +3,6 @@ console.log(fixedNumber);
 let nome = prompt('Write your Name');
 let cognome = prompt('Write your Lastname');
 let colore = prompt('Write your favorite color');
-document.getElementById('password').innerHTML = nome + cognome + colore + fixedNumber;
+const testo = 'This is your Passowrd:';
+document.getElementById('password').innerHTML = testo + " " + nome + cognome + colore + fixedNumber;
 console.log(password);
