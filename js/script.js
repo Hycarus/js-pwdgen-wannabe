b85dfd72b464a0e5b3ce88ef9d676f839a2c2782
Hycarus <mirkomarasco08@gmail.com>
2023-10-06 14:10:55 +0200

    added "function" in js

--- js/script.js
+++ js/script.js
@@ -3,3 +3,5 @@ console.log(fixedNumber);
 let nome = prompt('Write your Name');
 let cognome = prompt('Write your Lastname');
 let colore = prompt('Write your favorite color');
+document.getElementById('password').innerHTML = nome + cognome + colore + fixedNumber;
+console.log(password);
