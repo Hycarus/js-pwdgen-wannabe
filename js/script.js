7d6e91edc02125379fd89276795e698797c5c53e
Hycarus <mirkomarasco08@gmail.com>
2023-10-06 14:10:04 +0200

    added variable in js

---
+++ js/script.js
@@ -0,0 +1,5 @@
+const fixedNumber = 21;
+console.log(fixedNumber);
+let nome = prompt('Write your Name');
+let cognome = prompt('Write your Lastname');
+let colore = prompt('Write your favorite color');
