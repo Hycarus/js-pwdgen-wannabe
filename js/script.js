b2fb05a3798d9af734373bf9bf48ac195490d516
Hycarus <mirkomarasco08@gmail.com>
2023-10-06 15:12:57 +0200

    added reset button

--- js/script.js
+++ js/script.js
@@ -3,6 +3,5 @@ console.log(fixedNumber);
 let nome = prompt('Write your Name');
 let cognome = prompt('Write your Lastname');
 let colore = prompt('Write your favorite color');
-const testo = 'This is your Passowrd:';
-document.getElementById('password').innerHTML = testo + " " + nome + cognome + colore + fixedNumber;
+document.getElementById('password').innerHTML = nome + cognome + colore + fixedNumber;
 console.log(password);
